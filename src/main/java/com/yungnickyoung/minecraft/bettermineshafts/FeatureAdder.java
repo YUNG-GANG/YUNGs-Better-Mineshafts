67d2bfe8db8fc5c7dddb2e97d681642eb85e0ab6
Nicholas Young <ndy14@ufl.edu>
2020-04-05 21:26:41 -0400

    Fix vanilla mineshafts still spawning when using compiled jar.

--- src/main/java/com/yungnickyoung/minecraft/bettermineshafts/FeatureAdder.java
+++ src/main/java/com/yungnickyoung/minecraft/bettermineshafts/FeatureAdder.java
@@ -6,16 +6,14 @@ import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.decorator.Decorator;
 import net.minecraft.world.gen.decorator.DecoratorConfig;
-import net.minecraft.world.gen.feature.FeatureConfig;
-import net.minecraft.world.gen.feature.StructureFeature;
+import net.minecraft.world.gen.feature.*;
 
 import java.lang.reflect.Field;
+import java.util.List;
 import java.util.Map;
 
 public class FeatureAdder {
     public static void addBetterMineshafts(Biome biome) {
-        removeVanillaMineshaft(biome);
-
         // TODO - replace hardcoded spawnrates w/ config option
         BetterMineshaftFeatureConfig config = new BetterMineshaftFeatureConfig(0.004D, BetterMineshaftFeature.Type.NORMAL);
 
@@ -26,27 +24,18 @@ public class FeatureAdder {
             BetterMineshafts.BETTER_MINESHAFT_FEATURE.configure(config)
         );
 
-        biome.addFeature(
-            GenerationStep.Feature.UNDERGROUND_STRUCTURES,
-            BetterMineshafts.BETTER_MINESHAFT_FEATURE.configure(config)
-                .createDecoratedFeature(Decorator.NOPE.configure(DecoratorConfig.DEFAULT))
-        );
-    }
-
-    @SuppressWarnings("unchecked")
-    private static void removeVanillaMineshaft(Biome biome) {
-        /// Use reflection to access biome's structure feature list and remove vanilla mineshaft from it
         try {
-            Field field = biome.getClass().getSuperclass().getDeclaredField("structureFeatures");
-            field.setAccessible(true);
-            Map<StructureFeature<?>, FeatureConfig> structureFeatures =
-                (Map<StructureFeature<?>, FeatureConfig>)field.get(biome);
-            structureFeatures.keySet().removeIf(feature -> feature == BetterMineshafts.VANILLA_MINESHAFT_FEATURE);
-            field.set(biome, structureFeatures);
+            List<ConfiguredFeature<?, ?>> list = biome.getFeaturesForStep(GenerationStep.Feature.UNDERGROUND_STRUCTURES);
+            for (int i = 0; i < list.size(); i++) {
+                ConfiguredFeature<?, ?> feature = list.get(i);
+                if (((DecoratedFeatureConfig) (feature.config)).feature.feature instanceof MineshaftFeature) {
+                    list.set(i, BetterMineshafts.BETTER_MINESHAFT_FEATURE.configure(config).createDecoratedFeature(Decorator.NOPE.configure(DecoratorConfig.DEFAULT)));
+                }
+            }
         } catch (Exception e) {
             BetterMineshafts.LOGGER.warn(
                 String.format(
-                    "Encountered error attempting to remove vanilla mineshaft from biome %s: %s",
+                    "Encountered error attempting to replace vanilla mineshaft from biome %s: %s",
                     biome.getName().asString(),
                     e
                 )
