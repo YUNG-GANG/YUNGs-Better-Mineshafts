8825be7a43e5f30b821bbe6af5512a7fbef9fded
Nicholas Young <ndy14@ufl.edu>
2020-07-16 23:30:24 -0400

    Tweak world restart on config and update version

--- src/main/java/com/yungnickyoung/minecraft/bettermineshafts/config/Configuration.java
+++ src/main/java/com/yungnickyoung/minecraft/bettermineshafts/config/Configuration.java
@@ -97,63 +97,63 @@ public class Configuration {
 
     public static class Ores {
         @Config.Name("Enable Ore Deposits")
-        @Config.RequiresMcRestart
+        @Config.RequiresWorldRestart
         public boolean enabled = true;
 
         @Config.Name("Cobble Spawn Chance (Empty deposit)")
         @Config.Comment(
             "Chance of an ore deposit being cobblestone only.\n" +
             "Default: 50")
-        @Config.RequiresMcRestart
+        @Config.RequiresWorldRestart
         public int cobble = 50;
 
         @Config.Name("Coal Spawn Chance")
         @Config.Comment(
             "Chance of an ore deposit containing coal.\n" +
             "Default: 20")
-        @Config.RequiresMcRestart
+        @Config.RequiresWorldRestart
         public int coal = 20;
 
         @Config.Name("Iron Spawn Chance")
         @Config.Comment(
             "Chance of an ore deposit containing iron.\n" +
             "Default: 9")
-        @Config.RequiresMcRestart
+        @Config.RequiresWorldRestart
         public int iron = 9;
 
         @Config.Name("Redstone Spawn Chance")
         @Config.Comment(
             "Chance of an ore deposit containing redstone.\n" +
             "Default: 7")
-        @Config.RequiresMcRestart
+        @Config.RequiresWorldRestart
         public int redstone = 7;
 
         @Config.Name("Gold Spawn Chance")
         @Config.Comment(
             "Chance of an ore deposit containing gold.\n" +
             "Default: 7")
-        @Config.RequiresMcRestart
+        @Config.RequiresWorldRestart
         public int gold = 7;
 
         @Config.Name("Lapis Spawn Chance")
         @Config.Comment(
             "Chance of an ore deposit containing lapis.\n" +
             "Default: 3")
-        @Config.RequiresMcRestart
+        @Config.RequiresWorldRestart
         public int lapis = 3;
 
         @Config.Name("Emerald Spawn Chance")
         @Config.Comment(
             "Chance of an ore deposit containing emerald.\n" +
             "Default: 3")
-        @Config.RequiresMcRestart
+        @Config.RequiresWorldRestart
         public int emerald = 3;
 
         @Config.Name("Diamond Spawn Chance")
         @Config.Comment(
             "Chance of an ore deposit containing diamond.\n" +
             "Default: 1")
-        @Config.RequiresMcRestart
+        @Config.RequiresWorldRestart
         public int diamond = 1;
     }
 }
