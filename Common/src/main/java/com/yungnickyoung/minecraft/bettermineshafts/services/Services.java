682a598efa366b76f51de1a6520d307de520b6da
williambl <william@williambl.com>
2026-09-04 11:36:45 +0100

    Fix unsafe call to ServiceLoader#load
    See jaredlll08/MultiLoader-Template#97
    
    See YUNG-GANG/YUNGs-Better-Caves#198

--- Common/src/main/java/com/yungnickyoung/minecraft/bettermineshafts/services/Services.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/bettermineshafts/services/Services.java
@@ -9,7 +9,7 @@ public class Services {
     public static final IModulesLoader MODULES = load(IModulesLoader.class);
 
     public static <T> T load(Class<T> clazz) {
-        final T loadedService = ServiceLoader.load(clazz)
+        final T loadedService = ServiceLoader.load(clazz, Services.class.getClassLoader())
                 .findFirst()
                 .orElseThrow(() -> new NullPointerException("Failed to load service for " + clazz.getName()));
         BetterMineshaftsCommon.LOGGER.debug("Loaded {} for service {}", loadedService, clazz);
