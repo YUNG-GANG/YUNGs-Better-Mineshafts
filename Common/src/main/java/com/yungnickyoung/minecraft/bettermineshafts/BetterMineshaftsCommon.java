6dbb9d636cb142d4bef470cb90b0187466f7897a
williambl <william@williambl.com>
2026-09-10 14:01:51 +0100

    Switch to using new YUNG's API LocateReplacer API
    YUNG-GANG/YUNGs-Better-Desert-Temples#51

--- Common/src/main/java/com/yungnickyoung/minecraft/bettermineshafts/BetterMineshaftsCommon.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/bettermineshafts/BetterMineshaftsCommon.java
@@ -3,7 +3,14 @@ package com.yungnickyoung.minecraft.bettermineshafts;
 import com.yungnickyoung.minecraft.bettermineshafts.module.ConfigModule;
 import com.yungnickyoung.minecraft.bettermineshafts.services.Services;
 import com.yungnickyoung.minecraft.yungsapi.api.YungAutoRegister;
+import com.yungnickyoung.minecraft.yungsapi.api.world.structure.locate.LocateReplacer;
 import io.netty.util.internal.ConcurrentSet;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.tags.StructureTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.levelgen.structure.BuiltinStructures;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -23,5 +30,9 @@ public class BetterMineshaftsCommon {
     public static void init() {
         YungAutoRegister.scanPackageForAnnotations("com.yungnickyoung.minecraft.bettermineshafts.module");
         Services.MODULES.loadModules();
+
+        LocateReplacer.register(BuiltinStructures.MINESHAFT,
+                TagKey.create(Registries.STRUCTURE, Identifier.fromNamespaceAndPath(MOD_ID, "better_mineshafts")),
+                () -> CONFIG.disableVanillaMineshafts);
     }
 }
